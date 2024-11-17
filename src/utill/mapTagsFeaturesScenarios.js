c011c628182a31d77510f4d198c6baf2d96de5b1
sandhani <30631105+SandHani45@users.noreply.github.com>
2024-11-17 15:15:35 +0400

    Create mapTagsFeaturesScenarios.js

---
+++ src/utill/mapTagsFeaturesScenarios.js
@@ -0,0 +1,25 @@
+const mapTagsFeaturesScenarios = (jsonData) => {
+  console.log('----------jsonData', jsonData);
+  const { TagLine: tagLines, ScenarioLine: scenarioLine, StepLine: stepLine } = jsonData;
+  const mapping = {};
+
+  tagLines.forEach((tagLine) => {
+    const tag = tagLine.data;
+    const scenarioIds = tagLine.properties.scenarioIds || [];
+    if (!mapping[tag]) {
+      mapping[tag] = { scenarioSteps: [] };
+    }
+    scenarioIds.forEach((id) => {
+      const filteredScenarios = scenarioLine.filter((scenario) => scenario.id === id);
+      const stepLines = stepLine.filter((step) => step.properties.scenarioId === id);
+      const mapStepAndScenarios = filteredScenarios.map((item) => ({
+        ...item,
+        stepLines,
+      }));
+      mapping[tag].scenarioSteps.push(...mapStepAndScenarios);
+    });
+  });
+  return mapping;
+};
+
+export default mapTagsFeaturesScenarios;
