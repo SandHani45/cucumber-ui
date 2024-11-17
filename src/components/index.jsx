836e533b2c66703369c8279357028eed852248cf
sandhani <30631105+SandHani45@users.noreply.github.com>
2024-11-17 15:11:05 +0400

    Create index.jsx

---
+++ src/components/index.jsx
@@ -0,0 +1,11 @@
+import Header from "./Header";
+import Terminal from "./Terminal";
+import SelectDropdown from "./SelectDropdown";
+import Chips from "./Chips";
+import Layout from "./Layout";
+import Card from "./Card";
+import Accordion from "./Accordion";
+import StepLine from "./StepLine";
+import StringInput from './StringInput';
+import AgTable from "./AgTable";
+export { Terminal, Header, SelectDropdown, Chips, Layout, Card, Accordion, StepLine, StringInput, AgTable };
