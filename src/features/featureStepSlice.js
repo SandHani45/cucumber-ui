df9b786b103d5b765bd8068037dd1ee2d02c5a9c
sandhani <30631105+SandHani45@users.noreply.github.com>
2024-11-17 15:11:55 +0400

    Create featureStepSlice.js

---
+++ src/features/featureStepSlice.js
@@ -0,0 +1,53 @@
+import { createSlice, createAsyncThunk, isAction } from '@reduxjs/toolkit';
+import {featureStepService} from '../services/featureStep.service';
+import mapTagsFeaturesScenarios from '../utill/mapTagsFeaturesScenarios';
+
+// Async thunk for fetching tags
+export const fetchFeatureStepSlice = createAsyncThunk('tags/fetchFeatureSteps', async () => {
+  return await featureStepService();
+});
+
+// Initial state
+const initialState = {
+  featureStep: [],
+  tagsList:[],
+  mapTagsFeaturesScenariosData: [],
+  loading: false,
+  error: null,
+};
+
+const featureStepSlice = createSlice({
+  name: 'featureStep',
+  initialState,
+  reducers: {
+    updateTagIsActive: (state, action) => {
+      state.tagsList = state.tagsList.map(tag => 
+          tag.id === action.payload ? { ...tag, isActive: !tag.isActive } : tag
+        );
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchFeatureStepSlice.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchFeatureStepSlice.fulfilled, (state, action) => {
+        state.loading = false;
+        state.featureStep = action.payload;
+        state.tagsList = action.payload.TagLine.map((tags)=>{
+          return {
+            ...tags, isAction: false
+          }
+        })
+        console.log('----mapTagsFeaturesScenarios(action.payload)', mapTagsFeaturesScenarios(action.payload))
+        state.mapTagsFeaturesScenariosData = mapTagsFeaturesScenarios(action.payload)
+      })
+      .addCase(fetchFeatureStepSlice.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  },
+});
+export const { updateTagIsActive } = featureStepSlice.actions;
+export default featureStepSlice.reducer;
