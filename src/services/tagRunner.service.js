c4874216507c60a1726ede1e256c527297d4fcc7
sandhani <30631105+SandHani45@users.noreply.github.com>
2024-11-17 15:14:54 +0400

    Create tagRunner.service.js

---
+++ src/services/tagRunner.service.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+import stepMock from './../mock/stepsMock.json'
+const tagRunnerService = async () => {
+  try {
+    // const response = await axios.get(`${BASE_URL}/tags`);
+    // const res = response.data;
+    return {
+        tag: '@ui',
+        started: true
+    }
+  } catch (error) {
+    console.error("Error fetching feature Step :", error);
+    throw error;
+  }
+};
+
+export { tagRunnerService };
