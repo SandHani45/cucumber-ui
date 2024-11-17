7d5b6724e7a28f5d4153418a02f038b4e8d14778
sandhani <30631105+SandHani45@users.noreply.github.com>
2024-11-17 15:26:50 +0400

    Create store.js

---
+++ src/store.js
@@ -0,0 +1,16 @@
+  import { configureStore } from '@reduxjs/toolkit';
+  import exampleSlice from './features/exampleSlice';
+  import tagsReducer from './features/tagsSlice';
+  import featureStepReducer from './features/featureStepSlice'
+  import globalSlice from './features/globalSlice';
+
+  const store = configureStore({
+    reducer: {
+      exampleList: exampleSlice,
+      tags: tagsReducer,
+      featureStep: featureStepReducer,
+      globalState: globalSlice
+    },
+  });
+
+  export default store;
